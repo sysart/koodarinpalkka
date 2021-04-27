c6b9ef838229a5658e833aa22eebf7de532126d6
Juhannuspukki <jere@jere.pro>
2021-04-27 15:54:30 +0300

    Update privacy policy link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
   <footer className="Footer">
     <img alt="logo" src="../logo.png" />
     <p className="Policies">
-      <a target="_blank" rel="noopener noreferrer" href="https://sysart.fi/wp-content/uploads/2018/09/Tietosuojaseloste_Sysart.fi-1.pdf">Tietosuojaseloste</a>
+      <a target="_blank" rel="noopener noreferrer" href="https://sysart.fi/wp-content/uploads/2021/04/Tietosuojaseloste_Sysart.fi-2021-1.pdf">Tietosuojaseloste</a>
       <a href="/evasteseloste">Evästeseloste</a><br/>
       <a target="_blank" rel="noopener noreferrer" href="https://sysart.fi/wp-content/uploads/2020/05/Rekisteriseloste-rekrytointi.pdf">Rekisteriseloste</a>
     </p>
